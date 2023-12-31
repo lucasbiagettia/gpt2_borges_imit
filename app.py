cfbb948ad4c0537096bede32f49ee83d6fe9b70f
lbiagetti <lucasbiagetti@gmail.com>
2023-12-30 21:35:01 -0300

    dolly model

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
-from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+import torch
+from transformers import pipeline
 
-def generar_texto_creativo(prompt, modelo, tokenizer, max_length=100, temperatura=1.0):
-    entrada_codificada = tokenizer.encode(prompt, return_tensors="pt")
+generate_text = pipeline(model="databricks/dolly-v2-3b", torch_dtype=torch.bfloat16, trust_remote_code=True, device="cpu")
 
-    salida = modelo.generate(
-        input_ids=entrada_codificada,
-        max_length=max_length,
-        temperature=temperatura,
-        num_beams=5,
-        no_repeat_ngram_size=2,
-        top_k=50,
-        top_p=0.95,
-        early_stopping=True
-    )
+print("Modelo cargado")
 
-    texto_generado = tokenizer.decode(salida[0], skip_special_tokens=True)
-    return texto_generado
+res = generate_text("Explain to me the difference between nuclear fission and fusion.")
+print(res[0]["generated_text"])
 
-# Cargar el modelo preentrenado GPT-Neo y su tokenizer
-modelo = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+prompt = "nada"
 
-# Generar texto creativo a partir de un prompt
-prompt = "En un mundo donde los robots han conquistado la Tierra"
-texto_generado = generar_texto_creativo(prompt, modelo, tokenizer, max_length=150, temperatura=0.7)
+while res != "fin":
+  prompt = input("Ingrese un número: ")
+  res = generate_text(prompt)
+  print(res[0]["generated_text"])
 
-print(texto_generado)
+print("fin")
